ee826a8465bdd8d00ce16e54a1ff98f33cd4b3fa
chinhung <864336723@qq.com>
2022-02-20 17:12:01 +0800

    菜单布局引入

---
+++ src/components/common/left-menu.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- 侧边栏菜单 -->
+  <el-menu
+    background-color="#333744"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <el-submenu index="1">
+      <template slot="title">
+        <!-- 图标 -->
+        <i class="el-icon-location"></i>
+        <!-- 文本 -->
+        <span>导航一</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="1-1">
+          <!-- 图标 -->
+          <i class="el-icon-location"></i>
+          <!-- 文本 -->
+          <span>导航一</span>
+        </el-menu-item>
+        <el-menu-item index="1-2">
+          <!-- 图标 -->
+          <i class="el-icon-location"></i>
+          <!-- 文本 -->
+          <span>导航二</span>
+					</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item index="1-3">选项3</el-menu-item>
+      <el-submenu index="1-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="1-4-1">选项1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+  </el-menu>
+</template>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,22 +1,27 @@
 <template>
   <el-container>
-    <el-header
-      >
-			<div>
-				<img src="@/assets/logo.png" alt="">
-				<span>Genshin Impact Center</span>
-			</div>
+    <el-header>
+      <div>
+        <img src="@/assets/logo.png" alt="" />
+        <span>Genshin Impact Center</span>
+      </div>
       <el-button @click="logout" type="info">退出</el-button>
     </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <left-menu />
+      </el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
+import leftMenu from "@/components/common/left-menu.vue";
 export default {
+  components: {
+    leftMenu,
+  },
   methods: {
     logout() {
       window.sessionStorage.clear();
@@ -27,32 +32,32 @@ export default {
 
 <style scoped>
 .el-container {
-	height: 100%;
+  height: 100%;
 }
 .el-header {
-	background-color: #373d41;
-	display: flex;
-	justify-content: space-between;
-	padding-left: 0;
-	align-items: center;
-	color: #fff;
-	font-size: 20px;
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
 }
 .el-header div {
-	display: flex;
-	align-items: center ;
+  display: flex;
+  align-items: center;
 }
 .el-header div span {
-	margin-left: 15px;
+  margin-left: 15px;
 }
 img {
-	width: 100px;
-	height: 60px;
+  width: 100px;
+  height: 60px;
 }
 .el-aside {
-	background-color: #333744;
+  background-color: #333744;
 }
 .el-main {
-	background-color: #eaedf1;
+  background-color: #eaedf1;
 }
 </style>
